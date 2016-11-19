44f586d904121a91e4d692b20c8c4a669a1954c1
Vinchenso <chensok@gmail.com>
2016-11-19 10:30:54 +0200

    Completed 4: Ember Routes

--- app/router.js
+++ app/router.js
@@ -8,6 +8,17 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
+  this.route('bookmarks', function() {
+    this.route('new');
+
+    this.route('edit', {
+      path: '/edit/:bookmark_id'
+    });
+  });
+
+  this.route('page-not-found', {
+    path: '/*wildcard'
+  });
 });
 
 export default Router;
