3316fb536554e902d86bec601f52c0c94f696a02
Vinchenso <chensok@gmail.com>
2016-12-06 14:28:24 +0200

    jslint errors

--- app/routes/bookmarks/about.js
+++ app/routes/bookmarks/about.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.get('store').findRecord('bookmark',1)
+    return this.get('store').findRecord('bookmark',1);
     //this.get('store').queryRecord('bookmark',{aboutIsNull:true})
   }
 });
